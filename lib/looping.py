2788b507db2cd415f0c75264df1bf528db3ea9fe
florence <loracodes3@gmail.com>
2024-12-05 21:47:58 +0300

    for looping

--- lib/looping.py
+++ lib/looping.py
@@ -14,3 +14,14 @@ def square_integers(int_list):
     return [x ** 2 for x in int_list]
 
 
+def fizzbuzz():
+
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
